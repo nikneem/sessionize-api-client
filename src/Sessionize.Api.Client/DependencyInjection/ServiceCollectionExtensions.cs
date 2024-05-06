e8ecbc205d5cf7ac039bfe267f75d90dfd29a884
Eduard Keilholz <e.keilholz@4dotnet.nl>
2024-05-06 13:38:32 +0200

    #9 - Making the configuration section name adjustable

--- src/Sessionize.Api.Client/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/Sessionize.Api.Client/DependencyInjection/ServiceCollectionExtensions.cs
@@ -8,17 +8,18 @@ namespace Sessionize.Api.Client.DependencyInjection;
 
 public static class ServiceCollectionExtensions
 {
-
-
-    public static IServiceCollection AddSessionizeApiClient(this IServiceCollection services, IConfigurationBuilder configurationBuilder)
+    public static IServiceCollection AddSessionizeApiClient(
+        this IServiceCollection services, 
+        IConfigurationBuilder configurationBuilder,
+        string? configurationSectionName = null)
     {
-
+        var sectionName = configurationSectionName?? SessionizeConfiguration.SectionName;
         var configuration = configurationBuilder.Build();
         services.AddOptions<SessionizeConfiguration>()
-            .Bind(configuration.GetSection(SessionizeConfiguration.SectionName))
+            .Bind(configuration.GetSection(sectionName))
             .ValidateOnStart();
 
-        var sessionizeConfiguration = configuration.GetSection(SessionizeConfiguration.SectionName)
+        var sessionizeConfiguration = configuration.GetSection(sectionName)
             .Get<SessionizeConfiguration>();
 
         if (sessionizeConfiguration == null)

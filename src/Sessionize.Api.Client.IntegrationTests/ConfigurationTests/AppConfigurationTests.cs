9fb36ccc706d12c16c964270ad310e8d5bd1fe67
nikneem <ekeilholz+git@outlook.com>
2025-05-14 14:42:28 +0200

    Fix code coverage for sonar cloud (#17)
    * Fix code coverage for sonar cloud
    
    * Attempt
    
    * Restored original sonar task
    
    * Adding coverage report to artifact
    
    * Adding tests
    
    ---------
    
    Co-authored-by: Eduard Keilholz <e.keilholz@4dotnet.nl>

---
+++ src/Sessionize.Api.Client.IntegrationTests/ConfigurationTests/AppConfigurationTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Options;
+using Sessionize.Api.Client.Configuration;
+
+namespace Sessionize.Api.Client.IntegrationTests.ConfigurationTests;
+
+public class AppConfigurationTests
+{
+    [Fact]
+    public void WhenConfigurationMeetsMinimalRequirement_ConfigurationValidationSucceeds()
+    {
+        // Arrange
+        var config = new SessionizeConfiguration
+        {
+            BaseUrl = "https://sessionize.com"
+        };
+        var validation = new SessionizeConfigurationValidation();
+
+        // Act
+        var result = validation.Validate(null, config);
+
+        // Assert
+        Assert.Equal(ValidateOptionsResult.Success, result);
+    }
+
+    [Fact]
+    public void WhenConfigurationIsInvalid_ConfigurationFallsBackToDefault()
+    {
+        // Arrange
+        var config = new SessionizeConfiguration();
+        var validation = new SessionizeConfigurationValidation();
+
+        // Act
+        var result = validation.Validate(null, config);
+
+        // Assert
+        Assert.Equal(ValidateOptionsResult.Success, result);
+    }
+
+}
